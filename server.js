8378638a9879d7798352829a16f8cf1397d8d12b
Nathan louissaint <louissaintmark@gmail.com>
2023-04-28 22:51:25 -0400

    html and css

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post('/register',async(req,res)=>{
     try{
      const hashedPassword = await bcrypt.hash(req.body.password, 10)
      users.push({
-        id: Date.now.toString(),
+        id: Date.now().toString(),
         name:req.body.name,
         email:req.body.email,
         password:req.body.password
